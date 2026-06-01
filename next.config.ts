5f3695a5bb99398c677d93694d656e02ec85b09f
Ramkumar143z <ramcse142@gmail.com>
2026-06-01 12:59:44 +0530

    fix: explicitly set basePath and assetPrefix for GitHub pages in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? '/SEEDSTHERAPHY' : '',
+  assetPrefix: isProd ? '/SEEDSTHERAPHY/' : '',
 };
 
 export default nextConfig;
